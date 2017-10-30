8c0e4b984a4d0445e4c9c4ddc2b03b04847ac8ea
Dmitry Petrov <dpetroff@gmail.com>
2017-10-30 23:05:57 +0100

    move all generic functions to helpers

--- src/helpers.lisp
+++ src/helpers.lisp
@@ -29,6 +29,10 @@
                  (setf idx (mod (1+ idx) n))
               finally (return (nreverse res))))))
 
+(defun in-system-path (name extension)
+  (let ((fname (make-pathname :name name :type extension))
+        (asdf-location (asdf:system-source-file :wave-research)))
+    (merge-pathnames fname asdf-location)))
 
 (defun interleaved-to-arrays (array num-values &key (type 'single-float))
   (when (> (mod (length array) num-values) 0)
@@ -41,3 +45,32 @@
       (dotimes (value-idx num-values separated)
         (setf (aref separated value-idx frame)
               (aref array (+ (* frame num-values) value-idx)))))))
+
+(defun fill-buffer (buffer source start end)
+  (let ((src-len (length source))
+        (idx 0))
+    (loop while (< (+ idx start) end)
+          do (if (>= (+ idx start) src-len)
+                 (setf (aref buffer idx) 0.0)
+                 (setf (aref buffer idx) (aref source (+ idx start))))
+             (incf idx))
+    buffer))
+
+(defun audio-series (data)
+  (let ((ts 0.0)
+        (idx 0))
+    (loop while (< idx (length data))
+          collect (list ts (aref data idx))
+          do (incf ts +frame-duration+)
+             (incf idx 1))))
+
+
+(defun plot-png (pathname)
+  (let ((filename (namestring pathname)))
+    (swank:eval-in-emacs
+     `(slime-media-insert-image (create-image ,filename)
+                                ,filename))))
+
+(defmethod print-object :before ((obj pathname) stream)
+  (when (string= "png" (pathname-type obj))
+    (plot-png obj)))
--- src/wave-research.lisp
+++ src/wave-research.lisp
@@ -1,32 +1,9 @@
 (in-package #:wave-research)
 
-(defun in-system-path (name extension)
-  (let ((fname (make-pathname :name name :type extension))
-        (asdf-location (asdf:system-source-file :wave-research)))
-    (merge-pathnames fname asdf-location)))
-
 (defun test-file-path ()
   (let ((fname (make-pathname :name "jumps" :type "wav"))
         (asdf-location (asdf:system-source-file :wave-research)))
     (merge-pathnames fname asdf-location)))
-
-(defun plot-png (pathname)
-  (let ((filename (namestring pathname)))
-    (swank:eval-in-emacs
-     `(slime-media-insert-image (create-image ,filename)
-                                ,filename))))
-
-(defmethod print-object :before ((obj pathname) stream)
-  (when (string= "png" (pathname-type obj))
-    (plot-png obj)))
-
-(defun audio-series (data)
-  (let ((ts 0.0)
-        (idx 0))
-    (loop while (< idx (length data))
-          collect (list ts (aref data idx))
-          do (incf ts +frame-duration+)
-             (incf idx 1))))
 
 (defun plot-audio-data (data)
   (let ((series (take-every 100 (audio-series data)))
@@ -47,16 +24,6 @@
 
 (defun read-test-audio-data ()
   (slice (interleaved-to-arrays (getf (caddr (read-test-file)) :chunk-data) 2) 0 t))
-
-(defun fill-buffer (buffer source start end)
-  (let ((src-len (length source))
-        (idx 0))
-    (loop while (< (+ idx start) end)
-          do (if (>= (+ idx start) src-len)
-                 (setf (aref buffer idx) 0.0)
-                 (setf (aref buffer idx) (aref source (+ idx start))))
-             (incf idx))
-    buffer))
 
 (defun analyze-test-audio ()
   (let* ((frames (read-test-audio-data))
